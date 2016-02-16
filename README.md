7de09d33d0ba1b34f8430796581522de60f13060
albertosouza <alberto.souza.99@gmail.com>
2016-02-16 16:19:17 -0200

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# we-plugin-field-phone
+
+> Plugin to add phone fields in we.js project
+
+# Features
+
+## Form fields:
+
+```js
+  type: "phone/cell"
+```
+
+## Model validations:
+
+
+
+# Links
+
+> * We.js site: http://wejs.org
+
+#Copyright and license
+
+Under [the MIT license](LICENSE).
---
+++ plugin.js
@@ -0,0 +1,40 @@
+/**
+ * Plugin.js file, set configs, routes, hooks and events here
+ *
+ * see http://wejs.org/docs/we/plugin
+ */
+var phone = require('phone');
+
+module.exports = function loadPlugin(projectPath, Plugin) {
+  var plugin = new Plugin(__dirname);
+  // set plugin configs
+  // plugin.setConfigs({
+  // });
+  // ser plugin routes
+  // plugin.setRoutes({
+  // });
+
+  plugin.fieldCellSetValue = function fieldCellSetValue(fieldName) {
+    return function setCellValue(val) {
+      if (val) {
+        this.setDataValue(fieldName, phone(val)[0]);
+      } else {
+        this.setDataValue(fieldName, val);
+      }
+    }
+  }
+
+  plugin.validCellField = function validCellField(value) {
+    if (value) {
+      var result = phone(value);
+
+      if (!result || !result.length) {
+        throw new Error('we-plugin-field-phone:phone.is.invalid');
+      }
+    }
+
+
+  }
+
+  return plugin;
+};
